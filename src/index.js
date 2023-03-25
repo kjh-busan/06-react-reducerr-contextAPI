89433d9a13b1b8e9bfbbeaee1829b2f043add60f
kjh-busan <picsmash7@gmail.com>
2023-03-25 19:26:39 +0900

    feat: refactoring contextAPI

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { AuthContextProvider } from "./store/auth-context";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loginHandler = (email, password) => {
+    setIsLoggedIn(true);
+    localStorage.setItem("isLoggedIn", "1");
+  };
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("isLoggedIn");
+  };
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {props.chidren}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
